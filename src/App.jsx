c43fb7108128c4fdbba21fc04813f2acd171840d
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-01-15 16:08:43 +0100

    adding css to navbar, creating Home page, changing common values in app.css

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { Routes,Route } from 'react-router-dom';
 import { NavBar } from './components/navbar/NavBar';
 import { Footer } from './components/footer/Footer';
 import { Header } from './components/header/Header';
+import { Home } from './pages/home/Home';
 function App() {
   return (
     <div className="App">
       <Header />
       <NavBar />
       <Routes>
-        
+        <Route index element={<Home />} />
+        <Route path='accueil' element={<Home />} />
       </Routes>
       <Footer />
     </div>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-export const NavBar = () => {
-  return (
-    <nav className="navbar">
-        <ul className="nav-container">
-            <li>Accueil</li>
-            <li>Paramètres utilisateurs</li>
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-        </ul>
-    </nav>
-  )
+import './NavBar.scss';
+
+export const NavBar = () => {
+    return (
+        <nav className="navbar-container">
+            <div class="navlink-container">
+                <input type="checkbox" class="toggle-btn"></input>
+                <div class="burger-menu"></div>
+                <ul className="menu">
+                    <li><NavLink to='accueil'className={(isActive)=>isActive ? 'active-nav' : undefined}>Accueil</NavLink></li>
+                    <li><NavLink  to=''className={(isActive)=>isActive ? '' : undefined}>Paramètres</NavLink></li>
+                    <li><NavLink  to=''className={(isActive)=>isActive ? '' : undefined}>Mon profil</NavLink></li>
+                    <li><NavLink  to=''className={(isActive)=>isActive ? '' : undefined}>Mes kits</NavLink></li>
+                </ul>
+            </div>
+        </nav>
+    )
 }
