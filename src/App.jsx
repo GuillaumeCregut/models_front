2bc1c3d3c7b27f819a4ffb60a97d32e0af47bf18
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-01-15 10:32:16 +0100

    adding Header and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css';
 import { Routes,Route } from 'react-router-dom';
 import { NavBar } from './components/NavBar/NavBar';
+import { Footer } from './components/Footer/Footer';
+import { Header } from './components/Header/Header';
 function App() {
   return (
     <div className="App">
+      <Header />
       <NavBar />
       <Routes>
         
       </Routes>
+      <Footer />
     </div>
   );
 }
