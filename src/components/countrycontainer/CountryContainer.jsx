d349c6f1ca83883dd2fa370733dc2a26bd145591
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-02-03 19:44:13 +0100

    get API infos for countries

--- src/components/countrycontainer/CountryContainer.jsx
+++ src/components/countrycontainer/CountryContainer.jsx
@@ -1,9 +1,22 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './CountryContainer.scss';
 
 const CountryContainer = () => {
-  return (
-    <div>CountryContainer</div>
-  )
+    const [countries, setCountries]=useState([]);
+    useEffect(()=>{
+        const getCountries=async()=>{
+            const url=`${process.env.REACT_APP_API_URL}country`;
+            await axios.get(url)
+                .then(res=>{
+                    
+                    setCountries(res.data);});
+        }
+        getCountries();
+    },[]);
+    return (
+        <div>CountryContainer</div>
+    )
 }
 
 export default CountryContainer
