339c9da687ddc643d2f2d9d998a8a91f3f5b8820
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-02-04 16:25:35 +0100

    refactoring simpleCards

--- src/components/simplecardcontainer/SimpleCardContainer.jsx
+++ src/components/simplecardcontainer/SimpleCardContainer.jsx
@@ -5,21 +5,15 @@ import './SimpleCardContainer.scss';
 export const SimpleCardContainer = ({item, kind}) => {
   const [displayBack,setDisplayBack]=useState(false)
 
-  const turnCard=()=>{
-    setDisplayBack(!displayBack);
-  }
-  
   return (
-    <article className="flip-card" onClick={turnCard}>
-        <div className={displayBack? "flip-card-inner  flip-card-inner-down":"flip-card-inner"}>
-            <div className="flip-card-front">
+   <div  className="container-Card">
+  
+           <div className='flip-card-front'>
                  {item.name}
             </div>
-            <div className="flip-card-back">
+            <div className='flip-card-back'>
                 {kind}
             </div>
-        </div>
-    </article>
-    
+    </div>
   )
 }
