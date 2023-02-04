749be5765b2f7ccb127b540905b9ad13d0fd19d2
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-02-04 14:55:09 +0100

    adding loading display

--- src/components/countrycontainer/CountryContainer.jsx
+++ src/components/countrycontainer/CountryContainer.jsx
@@ -1,21 +1,22 @@
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { SimpleCardContainer } from '../simplecardcontainer/SimpleCardContainer';
 import { setCountry } from '../../feature/Country.slice';
 
 import './CountryContainer.scss';
+import { AwaitLoad } from '../awaitload/AwaitLoad';
 
 const CountryContainer = () => {
     const dispatch=useDispatch();
     const countriesData=useSelector((state)=>state.countries.country);
-
+    const [isLoaded,setIsLoaded]=useState(false);
     const getCountries=()=>{
         const url=`${process.env.REACT_APP_API_URL}country`;
          axios.get(url)
             .then((res)=>{
                 dispatch(setCountry(res.data))
-                //setCountries(res.data);
+                setIsLoaded(true);
             });
     }
     useEffect(()=>{
@@ -23,13 +24,15 @@ const CountryContainer = () => {
     },[]);
     return (
         <div className='country-container'>
-            {countriesData?.map(item=>(
+            {isLoaded?countriesData.map(item=>(
                 <SimpleCardContainer
                     key={item.id}
                     item={item}
                     kind='country'
                 />
-            ))}
+            ))
+            :<AwaitLoad />
+            }
         </div>
     )
 }
