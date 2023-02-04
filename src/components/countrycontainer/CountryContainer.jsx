f9d448a6caec20bfafa42efdb70a29dbf8be39d5
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-02-04 13:01:28 +0100

    adding store in countryContainer file

--- src/components/countrycontainer/CountryContainer.jsx
+++ src/components/countrycontainer/CountryContainer.jsx
@@ -1,16 +1,20 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { SimpleCardContainer } from '../simplecardcontainer/SimpleCardContainer';
 import './CountryContainer.scss';
 
 const CountryContainer = () => {
     const [countries, setCountries]=useState([]);
+    const dispatch=useDispatch();
+    const countriesData=useSelector((state)=>state.countries.country);
+
     useEffect(()=>{
         const getCountries=async()=>{
             const url=`${process.env.REACT_APP_API_URL}country`;
             await axios.get(url)
                 .then(res=>{
-                    
+                    dispatch(setCountries(res.data))
                     setCountries(res.data);});
         }
         getCountries();
