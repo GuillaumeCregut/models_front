bf9868cb4338aea4b3a71ed2e34efadb330c8bff
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-02-05 21:12:34 +0100

    adding style and removing useless element in signup

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,33 +1,37 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState} from 'react'
+import { useNavigate } from 'react-router-dom';
 
 import './Login.scss';
 
 const Login = (props) => {
     const [login, setLogin]=useState('');
     const[pass,setPass]=useState('');
-    const [validLogin, setvalidLogin]=useState(false);
-    const [validPass,setValidPass]=useState(false);
+    const [errMsg, setErrMsg]=useState(false);
+  
     const loginRef=useRef();
+    const errRef=useRef();
 
     useEffect(()=>{
         loginRef.current.focus();
     },[])
-    useEffect(()=>{
 
-    },[])
     useEffect(()=>{
+        setErrMsg('');
+    },[login, pass])
 
-    },[])
 
     const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
     const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
 
     const handleSubmit=(e)=>{
         e.preventDefault();
+        const url = `${process.env.REACT_APP_API_URL}auth/`;
     }
 
     return (
         <section className='login-container'>
+            <h2 className="login-title">Connexion</h2>
+            <p className={errMsg?"login-error-msg": "login-hide"} aria-live="assertive">{errMsg}</p>
             <form className='login-form' onSubmit={handleSubmit}>
                 <label htmlFor="login" className="login-label">Login :
                     <input 
@@ -35,6 +39,7 @@ const Login = (props) => {
                     className="input-login-form" 
                     id="login" 
                     required
+                    autoComplete='off'
                     ref={loginRef}
                     value={login}
                     onChange={(e)=>setLogin(e.target.value)}
@@ -42,7 +47,7 @@ const Login = (props) => {
                 </label>
                 <label htmlFor="password" className="login-label">Mot de passe :
                     <input 
-                    type="text" 
+                    type="password" 
                     className="input-login-form" 
                     id="password" 
                     required
@@ -50,7 +55,7 @@ const Login = (props) => {
                     onChange={(e)=>setPass(e.target.value)}
                     />
                 </label>
-                <button>Connexion</button>
+                <button className='login-btn'>Connexion</button>
             </form>
             <p className="login-footer">
                 Pas encore de compte ? Inscrivez-vous
