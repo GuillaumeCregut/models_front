8fa5b846a5d91220c71c4aa9df6f3d7ee0311397
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-02-05 21:21:56 +0100

    adding provider to index.js

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
 import store from "./app/store";
 import App from './App';
+import { AuthProvider } from './context/auth.provider';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+  <AuthProvider>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </AuthProvider>
 );
