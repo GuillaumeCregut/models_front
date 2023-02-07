33e59b16b3ff89c11da13a5087e76783aded73b8
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-02-07 12:18:01 +0100

    fetching API OK, storing context OK

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect, useRef, useState} from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
 import jwt_decode from "jwt-decode";
@@ -7,78 +7,87 @@ import jwt_decode from "jwt-decode";
 import './Login.scss';
 
 const Login = (props) => {
-    const [login, setLogin]=useState('');
-    const[pass,setPass]=useState('');
-    const [errMsg, setErrMsg]=useState(false);
-    const {setAuth}=useAuth();
-    const loginRef=useRef();
-    const errRef=useRef();
+    const [login, setLogin] = useState('');
+    const [pass, setPass] = useState('');
+    const [errMsg, setErrMsg] = useState(false);
+    const { setAuth } = useAuth();
+    const loginRef = useRef();
+    const errRef = useRef();
 
-    useEffect(()=>{
+    useEffect(() => {
         loginRef.current.focus();
-    },[])
+    }, [])
 
-    useEffect(()=>{
+    useEffect(() => {
         setErrMsg('');
-    },[login, pass])
+    }, [login, pass])
 
-    const handleSubmit=(e)=>{
+    const handleSubmit = (e) => {
         e.preventDefault();
         const url = `${process.env.REACT_APP_API_URL}auth/`;
-        /*
-            //Check datas
-            //Create data
-            axios.post(url,data,{withCredentials:true})
-            .then((resp)=>{
-                if(resp?.data){
-                    const accessToken=response.data?.accessToken;
-                    //decode token to get rank, firstname, lastname
-                    var decoded = jwt_decode(token);
- 
-                    console.log(decoded);
-                    const user{
-                        firstname,
-                        lastname,
-                        rank,
-                        token:accessToken
+        if (login === '' || pass === '') {
+            alert('Veuillez remplir tous les champs');
+        }
+        else {
+            const data = {
+                login: login,
+                password: pass
+            }
+            axios.post(url, data, { withCredentials: true })
+                .then((resp) => {
+                    if (resp?.data) {
+                        const token = resp.data?.accessToken;
+                        var decoded = jwt_decode(token);
+                        const user = {
+                            firstname: decoded.firstname,
+                            lastname: decoded.lastname,
+                            rank: decoded.rank,
+                            token: token
+                        }
+                        console.log(user)
+                        setAuth(user);
                     }
-                    setAuth(user);
-                }
-            })
-            .catch((err)=>{
-                    setErrMsg('');
+                })
+                .catch((err) => {
+                    let errorMessage = '';
+                    switch (err.response.status) {
+                        case 401: errorMessage = "Login ou mot de passe invalide";
+                            break;
+                        case 404: errorMessage = "Utilisateur inexistant";
+                            break;
+                        case 422: errorMessage = "Erreur dans les données fournies";
+                    }
+                    setErrMsg(errorMessage);
                     errRef.current.focus();
-            })
-
-
-        */
+                })
+        }
     }
 
     return (
         <section className='login-container'>
             <h2 className="login-title">Connexion</h2>
-            <p className={errMsg?"login-error-msg": "login-hide"} aria-live="assertive">{errMsg}</p>
+            <p className={errMsg ? "login-error-msg" : "login-hide"} aria-live="assertive" ref={errRef}>{errMsg} </p>
             <form className='login-form' onSubmit={handleSubmit}>
                 <label htmlFor="login" className="login-label">Login :
-                    <input 
-                    type="text" 
-                    className="input-login-form" 
-                    id="login" 
-                    required
-                    autoComplete='off'
-                    ref={loginRef}
-                    value={login}
-                    onChange={(e)=>setLogin(e.target.value)}
+                    <input
+                        type="text"
+                        className="input-login-form"
+                        id="login"
+                        required
+                        autoComplete='off'
+                        ref={loginRef}
+                        value={login}
+                        onChange={(e) => setLogin(e.target.value)}
                     />
                 </label>
                 <label htmlFor="password" className="login-label">Mot de passe :
-                    <input 
-                    type="password" 
-                    className="input-login-form" 
-                    id="password" 
-                    required
-                    value={pass}
-                    onChange={(e)=>setPass(e.target.value)}
+                    <input
+                        type="password"
+                        className="input-login-form"
+                        id="password"
+                        required
+                        value={pass}
+                        onChange={(e) => setPass(e.target.value)}
                     />
                 </label>
                 <button className='login-btn'>Connexion</button>
