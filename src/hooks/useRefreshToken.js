e0ef09d5c4a4673bd5f2942d2591d2ffca3c3d8e
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-02-07 17:50:56 +0100

    adding but not completed useRefreshToken

---
+++ src/hooks/useRefreshToken.js
@@ -0,0 +1,21 @@
+import useAuth from "./useAuth"
+import axios from 'axios';
+const useRefreshToken=()=>{
+    const {setAuth}=useAuth();
+
+    const refresh=async()=>{
+        let newToken='';
+        const url= `${process.env.REACT_APP_API_URL}refresh/`;
+        await axios
+        .get(url,{withCredentials:true})
+        .then((resp)=>{
+            console.log(resp.data);
+            //setAuth
+        })
+        .catch((err)=>{
+
+        })
+    }
+}
+
+export default useRefreshToken
