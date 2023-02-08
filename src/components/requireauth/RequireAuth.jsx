31de74e09593b3d07ec4403754d0de8fb8ed9139
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-02-08 21:43:45 +0100

    modify end point when accessing securised page

--- src/components/requireauth/RequireAuth.jsx
+++ src/components/requireauth/RequireAuth.jsx
@@ -9,7 +9,7 @@ const RequireAuth = ({allowedRoles}) => {
        
              auth?.rank===allowedRoles
             ?<Outlet />
-            :<Navigate to='/' state={{from:location}} replace /> 
+            :<Navigate to='/login' state={{from:location}} replace /> 
        
     )
 }
