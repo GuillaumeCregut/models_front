13ad1df314c1a1e9fb2a0e899a2fbc280a7ef2ae
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-02-08 22:32:59 +0100

    adding logout function

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -1,25 +1,36 @@
 import React from 'react'
 import useAuth from '../../hooks/useAuth';
 import {HiLockClosed, HiLockOpen} from 'react-icons/hi';
-
 import './Welcome.scss';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const Welcome = () => {
-    const {auth}=useAuth();
+    const {auth,setAuth}=useAuth();
     const {firstname,lastname}=auth;
     const navigate=useNavigate();
     const handleClick=()=>{
         
         if(auth?.token){
-            console.log('logout')
+            const url = `${process.env.REACT_APP_API_URL}logout/`;
+            axios
+            .get(url,{withCredentials:true})
+            .then((resp)=>{
+                if(resp.status===204){
+                    setAuth('');
+                    navigate('/');
+                }
+            })
+            .catch((err)=>{
+                setAuth('');
+            })
+
         }
         else{
             navigate('/login');
         }
     }
 
-    console.log('welcome: ',auth)
     return (
         <div className='welcome' onClick={handleClick}>
             {
