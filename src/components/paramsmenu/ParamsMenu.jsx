6763cd1c7c11da91a5fb1d9946fd550f78d6f22f
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-02-09 21:25:15 +0100

    create grid template and display

---
+++ src/components/paramsmenu/ParamsMenu.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './ParamsMenu.scss';
+const ParamsMenu = (props) => {
+    return (
+        <nav className="params-menu">
+            menus
+        </nav>
+    )
+}
+
+export default ParamsMenu
--- src/pages/params/Params.jsx
+++ src/pages/params/Params.jsx
@@ -1,9 +1,11 @@
 import CountryContainer from '../../components/countrycontainer/CountryContainer';
+import ParamsMenu from '../../components/paramsmenu/ParamsMenu';
 import './Params.scss';
 
 export const Params = () => {
   return (
-    <div>Params
+    <div className='params'>
+    <ParamsMenu />
       <CountryContainer />
     </div>
   )
