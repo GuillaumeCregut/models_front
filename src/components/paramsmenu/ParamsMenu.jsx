9168687b9a224eae7845e2de114c7b63659e5a52
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-02-09 22:01:08 +0100

    adding some css, moving welcome component

--- src/components/paramsmenu/ParamsMenu.jsx
+++ src/components/paramsmenu/ParamsMenu.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import './ParamsMenu.scss';
 const ParamsMenu = (props) => {
     return (
-        <nav className="params-menu">
-            menus
-        </nav>
+        <div className="params-menu">
+            <ul className="params-links-container">
+                <li className="link-param-item">Périodes</li>
+                <li className="link-param-item">Constructeurs</li>
+                <li className="link-param-item">Marques</li>
+                <li className="link-param-item">Types</li>
+                <li className="link-param-item">Echelles</li>
+                <li className="link-param-item">Pays</li>
+                <li className="link-param-item">Modèles</li>
+            </ul>
+        </div>
     )
 }
 
