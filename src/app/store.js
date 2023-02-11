22914039a5ebf555db2d0794e039a11bb9b3b4fa
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-02-11 13:22:47 +0100

    adding slice to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import {configureStore} from "@reduxjs/toolkit";
 import CountrySlice from "../feature/Country.slice";
+import  periodSlice  from "../feature/Period.slice";
 
 
 export default configureStore({
     reducer :{
         countries : CountrySlice,
+        periods : periodSlice,
        
     },
     //devTools :false //to avoid visible store in dev tools
