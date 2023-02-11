2ad34f7678213b05f1ad3977221047f7279db8be
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-02-11 19:51:16 +0100

    remove unused components

--- src/pages/profil/Profil.jsx
+++ src/pages/profil/Profil.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import Login from '../../components/login/Login'
-import SignUp from '../../components/signup/SignUp'
+
 
 export const Profil = () => {
   return (
     <div>
       Profil
-      {/* <SignUp /> */}
-      <Login />
+     
     </div>
   )
 }
