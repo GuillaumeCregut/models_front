27384c5fcda7ac2eae5a8ea1c0ffd5fcaac23f7c
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-02-12 22:10:45 +0100

    getting datas OK

--- src/components/buildercontainer/BuilderContainer.jsx
+++ src/components/buildercontainer/BuilderContainer.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
-const BuilderContainer = (props) => {
+const BuilderContainer = () => {
+    const url = `${process.env.REACT_APP_API_URL}builder`;
+    const [builders,setBuilders]=useState([]);
+
+    useEffect(()=>{
+        axios
+        .get(url)
+        .then((res)=>{
+            setBuilders(res.data)
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+    },[]);
     return (
         <div>
             Constructeurs
+            <ul>
+                {builders.map((item)=>(
+                    <li key={item.id}>{item.name} - {item.countryName}</li>
+                ))}
+            </ul>
         </div>
     )
 }
