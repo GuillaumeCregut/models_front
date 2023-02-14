9db7399e6962f702ea7dc2f6cd17692d06c0ec82
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-02-14 17:03:21 +0100

    creating scaleselector

---
+++ src/components/selectors/scaleselector/ScaleSelector.jsx
@@ -0,0 +1,48 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+import { useDispatch, useSelector } from "react-redux";
+import { setScale } from '../../../feature/Scale.slice';
+
+import './ScaleSelector.scss';
+
+const ScaleSelector = ({ id, selectedScale, setSelectedScale }) => {
+    const [scaleLoaded, setScaleLoaded] = useState(false);
+    const dispatch = useDispatch();
+    const scaleData = useSelector((state) => state.scales.scale);
+
+    useEffect(() => {
+        const getScales = async () => {
+            const url = `${process.env.REACT_APP_API_URL}scale`;
+            axios
+                .get(url)
+                .then((resp) => {
+                    dispatch(setScale(resp.data));
+                    setScaleLoaded(true);
+                })
+        }
+        if (!scaleData)
+            getScales();
+        else
+            setScaleLoaded(true);
+    }, []);
+
+    return (
+        <select
+            id={id}
+            value={selectedScale}
+            onChange={(e) => setSelectedScale(e.target.value)}
+            className='scale-selector'
+        >
+            {scaleLoaded
+                ? scaleData.map((item) => (
+                    <option
+                        key={item.id}
+                        value={item.id}>{item.name}</option>
+                ))
+                : null
+            }
+        </select>
+    )
+}
+
+export default ScaleSelector
