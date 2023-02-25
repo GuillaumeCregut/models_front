36ff8ad3a79f373f50a477217b04557482f062aa
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-02-25 22:11:43 +0100

    delete supplier OK

--- src/components/userprofil/supplierdetails/SupplierDetails.jsx
+++ src/components/userprofil/supplierdetails/SupplierDetails.jsx
@@ -1,8 +1,25 @@
+import useAxiosPrivate from "../../../hooks/useAxiosPrivate"
 
-const SupplierDetails = ({supplier}) => {
+const SupplierDetails = ({supplier, setSuppliers,suppliers}) => {
+    const axiosPrivate=useAxiosPrivate();
+    const handleDelete=()=>{
+        const id=supplier.id;
+        if(window.confirm("Voulez-vous supprimer ce fournisseur ?")){
+            const url = `${process.env.REACT_APP_API_URL}supplier/${supplier.id}`;
+            axiosPrivate
+                .delete(url)
+                .then((resp)=>{
+                    setSuppliers(suppliers.filter((item)=>item.id!==id))
+                })
+                .catch((err)=>{
+                    console.error(err);
+                })
+        }
+    }
+
     return (
         <li>
-            {supplier.name}
+           <button onClick={handleDelete}>-</button> {supplier.name}
         </li>
     )
 }
--- src/components/userprofil/usersupplier/UserSupplier.jsx
+++ src/components/userprofil/usersupplier/UserSupplier.jsx
@@ -59,6 +59,8 @@ const UserSupplier = () => {
                     <SupplierDetails
                         key={item.id}
                         supplier={item}
+                        suppliers={suppliers}
+                        setSuppliers={setSuppliers}
                     />
                 ))}
 
