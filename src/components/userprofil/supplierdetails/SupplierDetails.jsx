6b7bf761a5186ec42bbafed2f7b1219584c82e7b
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-02-25 22:56:21 +0100

    deleting and updating OK

--- src/components/userprofil/supplierdetails/SupplierDetails.jsx
+++ src/components/userprofil/supplierdetails/SupplierDetails.jsx
@@ -1,7 +1,17 @@
+import { useRef, useState } from "react";
 import useAxiosPrivate from "../../../hooks/useAxiosPrivate"
 
 const SupplierDetails = ({supplier, setSuppliers,suppliers}) => {
+    const [update,setUpdate]=useState(false);
+    const [newName,setNewName]=useState(supplier.name);
     const axiosPrivate=useAxiosPrivate();
+    const nameSupplier =useRef();
+    console.log(supplier);
+
+    const handleEdit=()=>{
+        setUpdate(prev=>!prev)
+    }
+
     const handleDelete=()=>{
         const id=supplier.id;
         if(window.confirm("Voulez-vous supprimer ce fournisseur ?")){
@@ -16,10 +26,38 @@ const SupplierDetails = ({supplier, setSuppliers,suppliers}) => {
                 })
         }
     }
+    const handleUpdate=()=>{
+        const id=supplier.id;
+        if(window.confirm("Voulez vous modifier ce fournisseur ?")){
+            const newSupplier={
+                name:newName,
+                owner: supplier.owner
+            }
+            const url = `${process.env.REACT_APP_API_URL}supplier/${supplier.id}`;
+            axiosPrivate
+                .put(url,newSupplier)
+                .then((resp)=>{
+                    setSuppliers(
+                        suppliers.map((item)=>{
+                            if(item.id===id)
+                                return {...item,name:newName}
+                            else
+                                return item;
+                        })
+                    )
+                    handleEdit();
+                })
+                .catch((err)=>{
+                    console.error(err)
+                })
+        }
+    }
 
     return (
         <li>
-           <button onClick={handleDelete}>-</button> {supplier.name}
+           {update
+           ? <p><input type="text" value={newName} onChange={(e)=>setNewName(e.target.value)}/> <button onClick={handleUpdate}>Valider</button> <button onClick={handleEdit}>Fermer</button></p>
+           :<div><button onClick={handleDelete}>-</button>{supplier.name} <button  onClick={handleEdit}>Modifier</button></div>}
         </li>
     )
 }
