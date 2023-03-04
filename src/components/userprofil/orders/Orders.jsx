b9d92379142605774d12c9900173318d6d7d3e56
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-03-04 19:10:15 +0100

    adding details on past orders, adding css

--- src/components/userprofil/orders/Orders.jsx
+++ src/components/userprofil/orders/Orders.jsx
@@ -1,17 +1,20 @@
 import { useState, useEffect, useRef } from 'react'
+import Popup from 'reactjs-popup';
 import useAuth from '../../../hooks/useAuth';
 import useAxiosPrivate from '../../../hooks/useAxiosPrivate';
 import ProviderSelector from '../../selectors/provideselector/ProviderSelector';
 import OrderModel from '../ordermodel/OrderModel';
 import ModelLine from './ModelLine';
+import OrderDetails from './orderdetails/OrderDetails';
 
 import './Orders.scss';
 
 const Orders = () => {
     const [orders, setOrders] = useState([]);
     const [listModel, setListModel] = useState([]);
-    const [refresh,setRefresh]=useState(false);
+    const [refresh, setRefresh] = useState(false);
     const [provider, setProvider] = useState(0);
+    const [deployed, setDeployed] = useState(false);
     const axiosPrivate = useAxiosPrivate();
     const orderRefRef = useRef();
     const { auth } = useAuth();
@@ -19,22 +22,22 @@ const Orders = () => {
     if (!idUser) {
         idUser = 0;
     }
-    useEffect(()=>{
-        try{
-            const ls=JSON.parse(window.localStorage.getItem("myOrder"));
-            if(ls?.provider){
+    useEffect(() => {
+        try {
+            const ls = JSON.parse(window.localStorage.getItem("myOrder"));
+            if (ls?.provider) {
                 setProvider(ls.provider)
             }
-            if(ls?.reference){
-                orderRefRef.current.value=ls.reference;
+            if (ls?.reference) {
+                orderRefRef.current.value = ls.reference;
             }
-            if(ls?.list)
+            if (ls?.list)
                 setListModel([...ls.list]);
         }
-        catch (err){
+        catch (err) {
             console.error(err)
         }
-    },[]);
+    }, []);
 
     useEffect(() => {
         const getOrders = () => {
@@ -42,7 +45,6 @@ const Orders = () => {
             axiosPrivate
                 .get(url)
                 .then((resp) => {
-                    console.log(resp.data)
                     setOrders(resp.data)
                 })
                 .catch((err) => {
@@ -52,21 +54,25 @@ const Orders = () => {
         getOrders();
     }, [refresh]);
 
-    useEffect(()=>{
-        if(listModel.length>0 || provider!==0){
-        const orderStore={
-            provider:parseInt(provider),
-            reference: orderRefRef?.current.value,
-            list: listModel
+    useEffect(() => {
+        if (listModel.length > 0 || provider !== 0) {
+            const orderStore = {
+                provider: parseInt(provider),
+                reference: orderRefRef?.current.value,
+                list: listModel
+            }
+            window.localStorage.setItem("myOrder", JSON.stringify(orderStore));
         }
-        window.localStorage.setItem("myOrder",JSON.stringify(orderStore));
-    }
-    },[listModel,provider]);
+    }, [listModel, provider]);
 
-    const resetForm=()=>{
-        //set provider 0,
+    const resetForm = () => {
+        setProvider(0);
+        setListModel([]);
+        orderRefRef.current.value = '';
+        window.localStorage.removeItem("myOrder")
         //Empty list
         //clear ref
+
     }
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -87,11 +93,12 @@ const Orders = () => {
                 if (window.confirm('voulez vous valider la commande ?')) {
                     const url = `${process.env.REACT_APP_API_URL}order/`;
                     axiosPrivate
-                        .post(url,dataSend)
-                        .then((resp)=>{
+                        .post(url, dataSend)
+                        .then((resp) => {
+                            resetForm();
                             setRefresh(!refresh)
                         })
-                        .catch((err)=>{
+                        .catch((err) => {
                             console.log(err)
                         })
                 }
@@ -128,50 +135,68 @@ const Orders = () => {
             setListModel([...newList]);
         } //On a pas le modele dans la liste, on le rajoute
         else {
-            setListModel([...listModel, { ...model,idModel: model.idModel, price: parseFloat(model.price), qtty: parseInt(model.qtty) }]);
+            setListModel([...listModel, { ...model, idModel: model.idModel, price: parseFloat(model.price), qtty: parseInt(model.qtty) }]);
         }
-       
+
     }
 
-    const setNewQtty=(id,newQtty)=>{
-        const modelMod=listModel.find((item=>item.idModel===id));
-        console.log(modelMod)
-        if(newQtty>0){
-            modelMod.qtty=newQtty;
-            setListModel(listModel.map((item)=>{
-                if(item.idModel===id)
+    const setNewQtty = (id, newQtty) => {
+        const modelMod = listModel.find((item => item.idModel === id));
+        if (newQtty > 0) {
+            modelMod.qtty = newQtty;
+            setListModel(listModel.map((item) => {
+                if (item.idModel === id)
                     return modelMod;
                 else return item;
             }))
         }
-        else{
-            setListModel(listModel.filter((item)=>item.idModel!==id))
+        else {
+            setListModel(listModel.filter((item) => item.idModel !== id))
         }
     }
 
     return (idUser !== 0
         ? (<section className='orders-container'>
-            Mes commandes :
+            Mes commandes : <button onClick={() => setDeployed(!deployed)}>{deployed ? 'cacher' : 'Afficher'}</button>
             {orders.length > 0
-                ? <ul>
+                ? <ul className={deployed ? "list-order list-order-deployed" : "list-order"}>
                     {orders.map((order) => (
-                        <li key={order.reference}>{order.reference}</li>
+                        <li key={order.reference}>
+                            Ref : <span className="item-list-order">{order.reference}</span> - Fournisseur : <span className="item-list-order">{order.providerName}</span>
+                            <Popup trigger={<button> Détails</button>} position="right center" modal className='popup'>
+                                <OrderDetails details={order} />
+                            </Popup>
+                        </li>
                     ))}
                 </ul>
                 : <p>Vous n'avez pas de commandes enregistrées</p>
             }
+            { /*
+            <Popup trigger={<button type="button">Ajouter un modèle à la commande</button>} position="right center" modal className='popupmodel'>
+                        <OrderModel
+                            addModel={addModel} />
+                    </Popup>
+
+        */}
+
             <div className="new-order-container">
                 <h2 className='new-order-form-title'>Ajouter une nouvelle commande</h2>
                 <form className='new-order-form' onSubmit={handleSubmit}>
-                    <label htmlFor="">Référence de la commande :
-                        <input type="text" id="" ref={orderRefRef} />
-                    </label>
-                    <label htmlFor="provider">Fournisseur :
-                        <ProviderSelector
-                            id="provider"
-                            provider={provider}
-                            setProvider={setProvider} />
-                    </label>
+                    <div className="form-header-inputs">
+                        <label htmlFor="">Référence de la commande :
+                            <input type="text" id="" ref={orderRefRef} />
+                        </label>
+                        <label htmlFor="provider">Fournisseur :
+                            <ProviderSelector
+                                id="provider"
+                                provider={provider}
+                                setProvider={setProvider} />
+                        </label>
+                    </div>
+                    <Popup trigger={<button type="button">Ajouter un modèle à la commande</button>} position="right center" modal className='popupmodel'>
+                        <OrderModel
+                            addModel={addModel} />
+                    </Popup>
                     <div className="model-list-added">
                         <table className='order-model-table'>
                             <caption>Résumé de la commande</caption>
@@ -186,16 +211,17 @@ const Orders = () => {
                             </thead>
                             <tbody>
                                 {listModel.map((model) => (
-                                    <ModelLine key={model.idModel} model={model} setNewQtty={setNewQtty}/>
+                                    <ModelLine key={model.idModel} model={model} setNewQtty={setNewQtty} />
                                 ))}
                             </tbody>
                         </table>
                     </div>
                     <button>Valider</button>
+
                 </form>
+
             </div>
-            <OrderModel
-                addModel={addModel} />
+
         </section>)
         : <p>Vous n'êtes pas connecté</p>
     )
