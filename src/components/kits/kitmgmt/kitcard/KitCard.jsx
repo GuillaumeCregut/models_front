ebc7a53ad96971c49e9d6121b1266021ed8476d0
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-03-05 21:37:27 +0100

    remove click to details

--- src/components/kits/kitmgmt/kitcard/KitCard.jsx
+++ src/components/kits/kitmgmt/kitcard/KitCard.jsx
@@ -4,12 +4,9 @@ import './KitCard.scss';
 const KitCard = ({kitDetails}) => {
     console.log(kitDetails);
     const url = `${process.env.REACT_APP_URL}`;
-    const handleClick=()=>{
-        alert('toto')
-    }
 
     return (
-        <div className='kit-card'  onClick={handleClick}>
+        <div className='kit-card'>
             <h4>{kitDetails.modelName}</h4>
             <img src={`${url}${kitDetails.boxPicture}`} alt={kitDetails.modelName}/>
             <p>{kitDetails.brandName} - {kitDetails.scaleName} </p>
