6e017727ab0c3196594f9818dbf266c6174c929e
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-03-05 18:40:03 +0100

    removing cslg in reducer

--- src/reducers/dragReducer.js
+++ src/reducers/dragReducer.js
@@ -3,13 +3,11 @@ import {produce} from "immer";
 export const dragReducer=produce((draft,action)=>{
     switch (action.type){
         case 'MOVE':{
-            console.log(`Moving '${action.itemId}' from ${action.from} to ${action.to}`)
             draft[action.from] = draft[action.from]||[];
             draft[action.to] = draft[action.to]||[];
             const [removed]=draft[action.from].splice(action.fromIndex,1);
             draft[action.to].splice(action.toIndex,0,removed);
         }
     }
-    // return state;
 });
 
