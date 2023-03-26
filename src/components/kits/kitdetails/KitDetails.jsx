19bbe9f1fbd9bbbd67bc3ea974d9de8f2896aa81
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-03-26 22:41:50 +0200

    display images when presents

--- src/components/kits/kitdetails/KitDetails.jsx
+++ src/components/kits/kitdetails/KitDetails.jsx
@@ -4,15 +4,21 @@ import useAuth from '../../../hooks/useAuth';
 import useAxiosPrivate from '../../../hooks/useAxiosPrivate';
 import { AwaitLoad } from '../../awaitload/AwaitLoad';
 import FileUploader from '../fileuploader/FileUploader';
+import Zoom from 'react-medium-image-zoom'
+import 'react-medium-image-zoom/dist/styles.css'
 
 import './KitDetails.scss';
+
+
+const MAX_FILE_UPLOAD = 4;
 
 const KitDetails = () => {
     const { id } = useParams();
     const axiosPrivate = useAxiosPrivate();
     const [modelDetail, setModelDetail] = useState({});
     const [isLoaded, setIsLoaded] = useState(false);
-    const [isError,setIsError]=useState(false);
+    const [isError, setIsError] = useState(false);
+    const [maxCount, setMaxCount] = useState(0);
     const urlDetail = `${process.env.REACT_APP_URL}`;
     const { auth } = useAuth();
     let idUser = auth?.id;
@@ -26,8 +32,13 @@ const KitDetails = () => {
             axiosPrivate
                 .get(url)
                 .then((resp) => {
-                   setModelDetail(resp.data);
-                   setIsLoaded(true);
+                    console.log(resp.data);
+                    setModelDetail(resp.data);
+                    let count = resp.data.pictures?.files.length;
+                    if (count) {
+                        setMaxCount(count);
+                    }
+                    setIsLoaded(true);
                 })
                 .catch((err) => {
                     console.error(err);
@@ -37,10 +48,11 @@ const KitDetails = () => {
         getModel();
     }, []);
 
-    const handleFiles=(files)=>{
+    const handleFiles = (files) => {
         console.log(files)
     }
 
+    console.log(maxCount)
     return (
         <div>
             {
@@ -52,7 +64,7 @@ const KitDetails = () => {
                                 <p>Période : {modelDetail.periodName} </p>
                                 <p>Catégorie : {modelDetail.categoryName}</p>
                                 <p>{modelDetail.brandName} - {modelDetail.scaleName} - {modelDetail.reference}</p>
-                                {modelDetail.picture?<p><img src={`${urlDetail}${modelDetail.picture}`} alt={modelDetail.modelName} className="detail-img"/></p>:null}
+                                {modelDetail.picture ? <p><img src={`${urlDetail}${modelDetail.picture}`} alt={modelDetail.modelName} className="detail-img" /></p> : null}
                                 {modelDetail.scalemates ? <a href={modelDetail.scalemates} no-referrer no-opener target='_blank'>Lien scalemates</a> : null}
                             </div>
                             {modelDetail.providerName
@@ -64,22 +76,38 @@ const KitDetails = () => {
                             <div className="picturebox">
                                 {
                                     modelDetail.pictures
-                                    ?<p>Images en stock</p>
-                                    :<p>Pas d'image en stock</p>
+                                        ? <ul className='picture-container'>
+                                            {modelDetail.pictures.files.map((file) => (
+                                                <li key={file} className='picture-item'>
+                                                    <Zoom>
+                                                        <img
+                                                            src={`${urlDetail}${modelDetail.pictures.baseFolder}${file}`}
+                                                            alt={file}
+                                                            className='img-model'
+                                                        />
+                                                    </Zoom>
+                                                </li>
+                                            )
+                                            )
+                                            }
+                                        </ul>
+                                        : <p>Pas d'image en stock</p>
                                 }
                             </div>
-                            <FileUploader
-                            label='Téléchargement'
-                            updateFilesCb ={handleFiles}
-                            multiple={true}
-                            maxFile={4}
-                            />
+                            {maxCount < MAX_FILE_UPLOAD
+                                ? <FileUploader
+                                    label='Téléchargement'
+                                    updateFilesCb={handleFiles}
+                                    multiple={true}
+                                    maxFile={MAX_FILE_UPLOAD}
+                                />
+                                : null}
                         </div>
                     </div>)
-                    : isError?(<p>Vous n'êtes pas autoriser à voir ce contenu. Contactez l'administrateur du site</p>):
-                    <AwaitLoad />
+                    : isError ? (<p>Vous n'êtes pas autoriser à voir ce contenu. Contactez l'administrateur du site</p>) :
+                        <AwaitLoad />
             }
-           <p> <Link to="/kits/finis">Retour à la page précédente</Link></p>
+            <p> <Link to="/kits/finis">Retour à la page précédente</Link></p>
         </div>
     )
 }
