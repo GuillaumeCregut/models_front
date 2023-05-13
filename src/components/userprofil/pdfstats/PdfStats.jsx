0945dbb7b45de49ee396d6c3a8759aa782cde774
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-05-13 16:15:27 +0200

    adding style to pdf page

--- src/components/userprofil/pdfstats/PdfStats.jsx
+++ src/components/userprofil/pdfstats/PdfStats.jsx
@@ -1,49 +1,51 @@
-import { useEffect,useState } from 'react';
+import { useEffect, useState } from 'react';
 import useAxiospdfBlob from '../../../hooks/useAxiospdfBlob';
 import useAuth from '../../../hooks/useAuth';
+import { AwaitLoad } from '../../awaitload/AwaitLoad';
 
+import './PdfStats.scss';
 
 const PdfStats = () => {
-    const axiosPrivate=useAxiospdfBlob();
-    const [path, setPath]=useState('');
-    const [numPages, setNumPages] = useState(null);
-    const [pageNumber, setPageNumber] = useState(1);
-    const {auth}=useAuth();
-    let idUser=auth?.id;
-    if(!idUser){
-      idUser=0;
+    const axiosPrivate = useAxiospdfBlob();
+    const [path, setPath] = useState('');
+    const { auth } = useAuth();
+    let idUser = auth?.id;
+    if (!idUser) {
+        idUser = 0;
     }
-    
 
-useEffect(()=>{
-    const getPdf=()=>{
-        const url = `${process.env.REACT_APP_API_URL}stats/${idUser}`;
-        axiosPrivate
-            .get(url)
-            .then((resp)=>{
-                const href=URL.createObjectURL(resp.data);
-                setPath(href)
-            })
-            .catch((err)=>{
-                console.error(err)
-            })
-    }
-    getPdf();
-},[])
+
+    useEffect(() => {
+        const getPdf = () => {
+            const url = `${process.env.REACT_APP_API_URL}stats/${idUser}`;
+            axiosPrivate
+                .get(url)
+                .then((resp) => {
+                    const href = URL.createObjectURL(resp.data);
+                    setPath(href)
+                })
+                .catch((err) => {
+                    console.error(err)
+                })
+        }
+        getPdf();
+    }, [])
 
     return (
-        <div>
-           {path===''?null:(
-           
-             <object data={path} type="application/pdf" width="800" height="500">
-          <a href={path} target='_blank'>PDF</a>
-            
-          </object>
-     
-          
-           )} 
-           Hello
-        </div>
+        <section className='pdf-container'>
+            <h1 className='pdf-title'>Statistiques</h1>
+            {path === ''
+                ? <AwaitLoad />
+                : (
+                    <div className="pdf-object-container">
+                        <object data={path} type="application/pdf" className='pdf-object'>
+                            <a href={path} target='_blank'>PDF</a>
+                        </object>
+                    </div>
+
+                )}
+
+        </section>
     )
 }
 
